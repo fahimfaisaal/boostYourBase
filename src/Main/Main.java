c4355b86e441e4ce58718059daaa1c9ced74f8cf
Fahim Faisal <fahimfaisal.soikot@gmail.com>
2021-07-05 09:44:20 +0600

    feat: test all sorting algos

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,5 @@
 package Main;
-import DSA.*;
+import Algorithms.*;
 
 import java.util.*;
 
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Searching algorithm
-        int [] array = {2, 30, 12, 3, 213, -1, -2, 40, 1, 23, 32, 3};
+        int [] array = {2, 30, -2, 12, 3, 213, -1, -2, 213, 40, 1, 23, 32, 3};
 
         int item = 213;
         // linear search - complexity O(n)
@@ -20,20 +20,27 @@ public class Main {
         // binary search - complexity O(log n)
         System.out.println("(binary search) index of " + item + " = " + SearchAlgo.binarySearch(array, item));
 
-        // Sorting Algorithm
+        // Sorting Algorithms
         System.out.println("Unsorted array: " + Arrays.toString(array));
 
+        SortWithAlgorithms sort = new SortWithAlgorithms(array);
+
         // bubble sort - complexity O(n^2)
-        // SortingAlgo.bubbleSort(array, 0, 0, array.length);
+        // System.out.println(
+        //    "(Bubble sort) Sorted array: " + Arrays.toString(sort.bubbleSort())
+        // );
 
-        // System.out.println("(Bubble sort) Sorted array: " + Arrays.toString(array));
+        // Selection sort - complexity O(n^2)
+        // System.out.println(
+        //    "(Selection sort) Sorted array: " + Arrays.toString(sort.selectionSort())
+        // );
 
-        // selection sort - complexity O(n^2)
-        System.out.println(
-            "(Selection sort) Sorted array: " + Arrays.toString(
-            SortingAlgo.selectionSort(array, 0, array.length)
-            )
-        );
+        // Insertion sort - complexity O(n^2)
+        // System.out.println(
+        //    "(Insertion sort) Sorted array: " + Arrays.toString(sort.insertionSort())
+        // );
 
+        // Quick Sort - complexity: worst case -> O(n^2) average case -> O(n log n)
+        System.out.println("(Quick sort) Sorted Array: " + sort.quickSort());
     }
 }
